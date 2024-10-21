fb771c05d5ffb6d6d0fef89e988b007b54884393
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2024-10-21 19:24:50 -0300

    Creando clases de camion y auto deportivo

--- src/Auto Deportivo.kt
+++ src/Auto Deportivo.kt
@@ -1,2 +1,8 @@
-class `Auto Deportivo` {
+class `Auto Deportivo`(
+    nombre: String,
+    velocidadMaxima: Int = 220,
+    combustible: Int = 80,
+    aceleracion: Int = 15,
+    frenado: Int = 10
+): VehiculoCustom(nombre, velocidadMaxima, combustible, aceleracion, frenado) {
 }
--- src/Camion.kt
+++ src/Camion.kt
@@ -1,2 +1,8 @@
-class Camion {
+class Camion(
+    nombre: String,
+    velocidadMaxima: Int = 90,
+    combustible: Int = 150,
+    aceleracion: Int = 5,
+    frenado: Int = 8,
+): VehiculoCustom(nombre, velocidadMaxima, combustible, aceleracion, frenado) {
 }
