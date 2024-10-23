ea675352a884d4e802bf56b7b9625a6a5ca972c8
Juanchipiruli <jbruera@facultad.sanfrancisco.utn.edu.ar>
2024-10-22 22:09:41 -0300

    plantilla funciones

--- src/PistaCustom.kt
+++ src/PistaCustom.kt
@@ -2,5 +2,13 @@ class PistaCustom(
     longitud: Int,
     clima: String
 ):Pista(longitud,clima) {
+    fun aplicarCondiciones(vehiculo: Vehiculo){
 
+    }
+    fun cambiarClima(){
+
+    }
+    fun aplicarEventoAleatorio(vehiculo: Vehiculo){
+
+    }
 }
